de6636b4ce26f5d23cc5e4a6ddb76921dc980aa5
alextaoultsides <alextaoultsides@gmail.com>
2018-09-15 23:20:27 -0400

    Add Pod Dependencies

--- Kairos/WeatherViewController.swift
+++ Kairos/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "f70d0f9f1c7b9688600a5d8b90323e3a"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
 
